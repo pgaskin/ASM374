536237b7df1ffdd228708e8e23775595741b5b49
Patrick Gaskin <19prg3@queensu.ca>
2023-03-01 19:54:52 -0500

    Prefer uppercase register names
    I was also asked to change this.

--- asm374.c
+++ asm374.c
@@ -392,26 +392,28 @@ typedef enum Reg {
 
 /**
  * GetReg gets the register name, returning NULL if the register does not exist.
+ *
+ * Note that uppercase names are preferred, although both will be accepted.
  */
 static const char *GetReg(Reg reg) {
     if (reg < RegCount) {
         switch (reg) {
-        case Reg_R0:   return "r0";
-        case Reg_R1:   return "r1";
-        case Reg_R2:   return "r2";
-        case Reg_R3:   return "r3";
-        case Reg_R4:   return "r4";
-        case Reg_R5:   return "r5";
-        case Reg_R6:   return "r6";
-        case Reg_R7:   return "r7";
-        case Reg_R8:   return "r8";
-        case Reg_R9:   return "r9";
-        case Reg_R10:  return "r10";
-        case Reg_R11:  return "r11";
-        case Reg_R12:  return "r12";
-        case Reg_R13:  return "r13";
-        case Reg_R14:  return "r14";
-        case Reg_R15:  return "r15";
+        case Reg_R0:   return "R0";
+        case Reg_R1:   return "R1";
+        case Reg_R2:   return "R2";
+        case Reg_R3:   return "R3";
+        case Reg_R4:   return "R4";
+        case Reg_R5:   return "R5";
+        case Reg_R6:   return "R6";
+        case Reg_R7:   return "R7";
+        case Reg_R8:   return "R8";
+        case Reg_R9:   return "R9";
+        case Reg_R10:  return "R10";
+        case Reg_R11:  return "R11";
+        case Reg_R12:  return "R12";
+        case Reg_R13:  return "R13";
+        case Reg_R14:  return "R14";
+        case Reg_R15:  return "R15";
         case RegCount: break;
         }
     }
@@ -1076,6 +1078,7 @@ int main(void) {
 int main(void) {
     fprintf(stderr, "> testing assembly\n");
     const char *asmtests[][2] = {
+        {"28918000", "and R1, R2, R3"},
         {"28918000", "and r1, r2, r3"},
         {"30918000", "or r1, r2, r3"},
         {"18228000", "add r0, r4, r5"},
